6bb78123dbd12cafd4003f8521490517422767a9
Joanna Vigne <joanna.vigne@fabernovel.com>
2022-10-12 16:33:24 +0200

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Joanna Vigné on 12/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
